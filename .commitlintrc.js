9d729c5d561b1d40f7028664c93b8a794c66ca1a
SoonIter <sooniter@gmail.com>
2023-03-21 22:17:40 +0800

    fix: pipeline release

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  ignores: [commit => commit?.startsWith?.('release')],
+  ignores: [
+    commit => commit?.startsWith?.('release'),
+    commit => commit?.startsWith?.('auto-generated-by-ci'),
+  ],
 };
